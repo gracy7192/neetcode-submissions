49caabc49533e9d484ad189c7929334083cb266b
Gracy <gracymahajan09022005@gmail.com>
2026-07-06 12:39:38 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxi=0;
+        int low=9999;
+        int high=0;
+        for(int i=0;i<prices.length;i++){
+          low=Math.min(low,prices[i]);
+          maxi=Math.max(maxi,prices[i]-low);
+        }
+        return maxi;
+    }
+}
